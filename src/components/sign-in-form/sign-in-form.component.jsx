56a069c319557f06fad2a5da316535be59f6de6c
Mandy_Li <mandy@digisalad.cool>
2023-10-10 13:55:13 +0800

    created authentication pafe component alongside sign in form

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+
+import { signInWithGooglePopup, createUserDocumentFromAuth,signInAuthUserWithEmailAndPassword } from "../../utils/firebase/firebase.utils";
+
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+import './sign-in-form.styles.scss'
+const defaultFormFields = {
+    email: '',
+    password: '',
+}
+const SignInForm = () => {
+    const [formFileds, setFormFileds] = useState(defaultFormFields)
+    const { email, password } = formFileds;
+
+    const resetFormFields = () => {
+        setFormFileds(defaultFormFields);
+    }
+    const signWithGoogle = async () => {
+        const {user} = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user)
+    }
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        try {
+            const res = await signInAuthUserWithEmailAndPassword(email,password);
+            console.log(res)
+            resetFormFields();
+        } catch (error) {
+            alert('incorrect password or email');
+            console.log(error.message);
+        }
+    }
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setFormFileds({ ...formFileds, [name]: value })
+    }
+    return (
+        <div className="sign-up-container">
+            <h2>Already have an account</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput label='Email' type="email" required onChange={handleChange} name="email" value={email} />
+                <FormInput label='Password' type="password" required onChange={handleChange} name="password" value={password} />
+                <div className="button-box">
+                    <Button type="submit">Sign In</Button >
+                    <Button type="button" buttonType='google' onClick={signWithGoogle}>Google Sign In</Button >
+                </div>
+            </form>
+        </div>
+    )
+}
+export default SignInForm;
--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -14,8 +14,6 @@ const defaultFormFields = {
 const SignUpForm = () => {
     const [formFileds, setFormFileds] = useState(defaultFormFields)
     const {displayName, email, password, confirmPassword} = formFileds;
-    
-    console.log(formFileds);
 
     const resetFormFields = ()=> {
         setFormFileds(defaultFormFields);
@@ -53,7 +51,7 @@ const SignUpForm = () => {
             <h2>Don't have an account</h2>
             <span>Sign up with your email and password</span>
             <form onSubmit={handleSubmit}>
-                <FormInput label='DisplayName' type="text" required onChange={handleChange} name="displayName" value={displayName} />
+                <FormInput label='Display Name' type="text" required onChange={handleChange} name="displayName" value={displayName} />
                 <FormInput label='Email' type="email" required onChange={handleChange} name="email" value={email}/>
                 <FormInput label='Password' type="password" required onChange={handleChange} name="password" value={password}/>
                 <FormInput label='Confirm Password' type="password" required onChange={handleChange} name="confirmPassword" value={confirmPassword}/>
