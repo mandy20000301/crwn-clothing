d515869b85e069be57697da3896f4088a113975f
Mandy_Li <mandy@digisalad.cool>
2023-10-22 15:26:54 +0800

    useNavigate

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,13 +1,16 @@
-import { useContext, Fragment } from 'react';
+import { useContext } from 'react';
 import { CartContext } from '../../contexts/cart.context';
-import { Outlet, Link } from "react-router-dom";
+import { useNavigate } from 'react-router-dom';
 import './cart-dropdown.styles.scss';
 import Button from '../button/button.component';
 import CartItem from '../cart-item/cart-item.component';
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
+    const navigate = useNavigate();
+    const goToCheckoutHandler = () => {
+        navigate('/checkout')
+    }
     return (
-        // <Fragment>
             <div className='cart-dropdown-container'>
                 <div className="cart-items">
                     {cartItems.map((item) => (
@@ -15,12 +18,8 @@ const CartDropdown = () => {
                     )
                     )}
                 </div>
-                <Link to='checkout'>
-                    <Button>GO TO CHECKOUT</Button>
-                </Link>
-                {/* <Outlet /> */}
+                    <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
             </div>
-        // </Fragment>
     )
 }
 
