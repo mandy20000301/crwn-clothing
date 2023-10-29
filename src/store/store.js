128691159bb5a5755a4c783a7f60b7f4fb5dce68
Mandy_Li <mandy@digisalad.cool>
2023-10-29 14:17:06 +0800

    store

---
+++ src/store/store.js
@@ -0,0 +1,11 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+
+import { rootReducer } from "./root-reducer";
+
+const midderWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...midderWares));
+
+export const store = createStore(rootReducer, undefined, composedEnhancers);
+
